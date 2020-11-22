86aa369067020bc8f0a84268a7af4cf959020730
Nazarchik <nazarkobernyk@MacBook-Pro.Home>
2020-11-22 22:47:57 +0000

    lessons 7 done

--- less 7/2/js/script.js
+++ less 7/2/js/script.js
@@ -35,40 +35,50 @@
 let textArea = document.querySelector(".textarea");
 
 window.addEventListener("keydown", (event) => {
+ 
+
   if (event.keyCode === 13) {
-  textArea.textContent+=('\n');
+    textArea.textContent += "\n";
   } else if (event.keyCode === 9) {
     event.preventDefault();
     document
       .querySelector("textarea")
       .append("\u00A0" + "\u00A0" + "\u00A0" + "\u00A0");
-  }else if (event.keyCode===16){
-    !textArea.textContents
-  }
-  else if (event.keyCode===20){
-    textArea.textContent.toUpperCase()  
-  }
-  else if (event.keyCode === 8 || event.code === "Backspace") {
-    let del = textArea.textContent.substring(0, textArea.textContent.length - 1);
-    textArea.textContent = del
-  }
- else {
+  } else if (event.keyCode === 16) {
+    !textArea.textContents;
+  } else if (event.keyCode === 20) {
+    textArea.textContent.toUpperCase();
+  } else if (event.keyCode === 8 || event.code === "Backspace") {
+    let del = textArea.textContent.substring(
+      0,
+      textArea.textContent.length - 1
+    );
+    textArea.textContent = del;
+  } 
+  else if(event.keyCode===32){
+    event.preventDefault();
+    document
+      .querySelector("textarea")
+      .append("\u00A0");
+  }else {
     textArea.textContent += event.key.toString();
   }
   document
-    .querySelector('#keyboard .fin[data="' + event.keyCode + '"]')
+    .querySelector('.conteiner-keys .fin[data="' + event.keyCode + '"],[data="' + event.code + '"]')
     .classList.add("silver");
 });
-window.addEventListener("keypress",(event)=>{
-  if (event.keyCode===16){
-    textArea.textContent.toUpperCase()
+window.addEventListener("keypress", (event) => {
+  if (
+    (event.code === "ShihtLeft") ||
+    event.code === "ShiftRight"
+  ) {
+    textArea.textContent.toUpperCase();
+  } else {
+    textArea.textContent.toLowerCase();
   }
-  else{
-    textArea.textContent.toLowerCase()
-  }
-} )
+});
 window.addEventListener("keyup", (event) => {
   document
-    .querySelector('#keyboard .fin[data="' + event.keyCode + '"]')
+    .querySelector('.conteiner-keys .fin[data="' + event.keyCode +'"],[data="' + event.code + '"]')
     .classList.remove("silver");
 });
