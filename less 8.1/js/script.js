490ebd21bcbbcbf2bd40d89d0c3a39fdad427a06
Nazarchik <nazarkobernyk@MacBook-Pro.Home>
2020-12-06 18:58:42 +0000

    update 75%

--- less 8.1/js/script.js
+++ less 8.1/js/script.js
@@ -1,16 +1,46 @@
-const firstName = document.forms['firstName'],
+const sub = document.forms['sub'],
+    signUp = document.querySelector('.container-fa'),
+    outUp = document.querySelector('.conteiner'),
+    firstName = document.forms['firstName'],
     secondName = document.forms['secondName'],
     emailAddress = document.forms['emailAddress'],
-    radio=document.forms['radio']
-    sub = document.forms['sub'];
+    fName = document.querySelector('.fName'),
+    sName = document.querySelector('.sName'),
+    eMail = document.querySelector('.eMail');
 
-sub.submit.addEventListener('click', (e) => {
-    e.preventDefault();
-    if (radio.lineR1===true){
-console.log(lineR1.value);    }
-    console.log(firstName.line1.value,secondName.line2.value,emailAddress.line3.value);
-    firstName.line1.value = ''
-    secondName.line2.value = ''
-    emailAddress.line3.value=''
 
+sub.submit.addEventListener("click", (e) => {
+    e.preventDefault()
+    fName.textContent = firstName.line1.value
+    sName.textContent = secondName.line2.value
+    eMail.textContent = emailAddress.line3.value
+
+
+    clikRadio()
+    clickSelect()
+
+    signUp.classList.add('viss')
+    outUp.classList.add('viss1')
 })
+
+
+function clikRadio() {
+    if (event.target.value === 'Woman') {
+        document.querySelector('.add-img').classList.remove('image-men')
+
+        document.querySelector('.add-img').classList.add('image-woman')
+    } else if (event.target.value === 'Man') {
+        document.querySelector('.add-img').classList.remove('image-woman')
+
+        document.querySelector('.add-img').classList.add('image-men')
+
+    }
+}
+
+function clickSelect(){
+
+       console.log(event.target)
+
+}
+
+
